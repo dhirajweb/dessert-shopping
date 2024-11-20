e4a1a29f53a74688c98a280875b5d332dd51c933
dhiraj_fugetron <dhiraj.makhijani@fugetroncorp.com>
2024-11-20 15:52:01 +0530

    shimmer ui animation

--- src/components/common/ShimmerCard.jsx
+++ src/components/common/ShimmerCard.jsx
@@ -1,6 +1,8 @@
 const ShimmerCard = () => {
   return (
-    <div className="h-[300px] bg-slate-200 rounded-lg shadow-md col-span-12 sm:col-span-6 lg:col-span-4 xl:col-span-4"></div>
+    <div className="relative mt-4 h-[300px] bg-slate-300 rounded-lg shadow-md col-span-12 sm:col-span-6 lg:col-span-4 xl:col-span-4 animate-pulse">
+      <button className="absolute w-[120px] flex bg-white border -bottom-[34px] left-[50%] translate-x-[-50%] translate-y-[-50%] px-8 py-4 items-center rounded-full gap-2 animate-pulse"></button>
+    </div>
   );
 };
 
